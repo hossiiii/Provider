39f985157aebb0bd42a6049da9e1035268c87148
hossiiii <hossiiii0117@gmail.com>
2020-06-28 20:03:43 +0900

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_app/main_model.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final String kboyText = 'KBOY';
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("コリアンダー"),
-        ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              Text(
-                'KBOYさんじゃないよ！',
-              ),
-            ],
+      home: ChangeNotifierProvider<MainModel>(
+        create: (_) => MainModel(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("コリアンダー"),
+          ),
+          body: Consumer<MainModel>(builder: (context, model, child) {
+              return Center(
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      model.kboyText,
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                    RaisedButton(
+                      child: Text('ボタン'),
+                      onPressed:(){
+                        //TODO
+                        model.changeKboyText();
+                      },
+                    ),
+                  ],
+                ),
+              );
+            }
           ),
         ),
       ),
