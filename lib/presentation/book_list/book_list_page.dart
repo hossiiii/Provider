f000aa190628955eddb4cd11aa2c1369c2322f28
hossiiii <hossiiii0117@gmail.com>
2020-06-29 19:26:49 +0900

    FirebaseのFirestoreに値を追加する（try catchの使い方も）

---
+++ lib/presentation/book_list/book_list_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_app/presentation/add_book/add_book_page.dart';
+import 'book_list_model.dart';
+
+class BookListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<BookListModel>(
+      create: (_) => BookListModel()..fetchBooks(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("本一覧"),
+        ),
+        body: Consumer<BookListModel>(builder: (context, model, child) {
+          final books = model.books;
+          final listTiles = books
+              .map(
+                (book) => ListTile(
+                  title: Text(book.title),
+                ),
+              )
+              .toList();
+          return ListView(
+            children: listTiles,
+          );
+        }),
+        floatingActionButton:
+            Consumer<BookListModel>(builder: (context, model, child) {
+          return FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () async {
+              //TODO
+              await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => AddBookPage(),
+                  fullscreenDialog: true,
+                ),
+              );
+              model.fetchBooks();
+            },
+          );
+        }),
+      ),
+    );
+  }
+}
