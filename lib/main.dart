337d99b4bbe47812f8a7e64cdc5588655b62d63b
hossiiii <hossiiii0117@gmail.com>
2020-06-29 17:30:41 +0900

    ModelからFirestoreのデータを取得する（mapの使い方も)

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
                       //TODO
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => BookList()),
+                        MaterialPageRoute(builder: (context) => BookListPage()),
                       );
                     },
                   ),
